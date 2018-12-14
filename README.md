4541c74eb2bdfbd745d0b2aa76e333b3096ad33a
Olivier Sallou <olivier.sallou@gmail.com>
2018-12-14 09:25:25 +0100

    add python client

--- README.md
+++ README.md
@@ -11,6 +11,22 @@ More an internal project, don't think it can be useful for others....
 
 Anyway, license is Apache 2.0
 
+## API
+
+See swagger.yaml
+
+## Client
+
+A python client is available as exemple to add software/version:
+
+
+    # Declare software
+    python softmngr.py --apikey=ADZDEEEFEF --url=https://xxx  --name=test --desc="sample test"
+    # Declare software and version
+    python softmngr.py --apikey=ADZDEEEFEF --url=https://xxx  --name=test --desc="sample test"  --version=1.0 --venv="/softs/local/env/envtest.sh" --vlocation="/softs/local/test"
+    # Add version to existing software
+    python softmngr.py --apikey=ADZDEEEFEF --url=https://xxx  --name=test --version=2.0 --venv="/softs/local/env/envtest-2.0.sh" --vlocation="/softs/local/test"
+
 ## Status
 
 In dev
---
+++ client/softmngr.py
@@ -0,0 +1,58 @@
+import requests
+import click
+import sys
+import json
+
+@click.command()
+@click.option('--apikey', help='api key [mandatory].')
+@click.option('--url', help='software manager url [mandatory].')
+@click.option('--name', help='software name [mandatory].')
+@click.option('--desc', help='software description.', default="")
+@click.option('--bio', help='bio.tools id.', default="")
+@click.option('--info', help='software additional info.', default="")
+@click.option('--version', help='version.')
+@click.option('--vinfo', help='version additional info.', default="")
+@click.option('--vtype', help='version installation type (manual, conda, ...).', default="")
+@click.option('--venv', help='env file/module to load.', default="")
+@click.option('--vlocation', help='version installation directory.', default="")
+def add(apikey, url, name, desc, bio, info, version, vinfo, vtype, venv, vlocation):
+    if not name:
+        print("name is mandatory")
+        sys.exit(1)
+    if not apikey:
+        print("Api key is mandatory")
+        sys.exit(1)
+    if not url:
+        print("Url is mandatory")
+        sys.exit(1)   
+    headers = {
+        'x-api-key': apikey
+    }
+    data = {
+        'software': {
+            'name': name,
+            'description': desc,
+            'info': info,
+            'uid': bio
+        }
+    }
+    if version:
+        data['version'] = {
+            'name': name,
+            'version': version,
+            'type': vtype,
+            'env': venv,
+            'location': vlocation,
+            'info': vinfo
+        }
+    
+    if not url.endswith('/soft'):
+        url += '/soft'
+    print("Send to " + url +"," + json.dumps(data))
+    r = requests.post(url, json = data, headers=headers)
+    if not r.status_code == 200:
+        print("something went wrong")
+        sys.exit(1)
+
+if __name__ == '__main__':
+    add()
